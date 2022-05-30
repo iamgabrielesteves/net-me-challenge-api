2ce38facb22c2a4ce7d3fddf2e703e3b6794494b
Gabriel Esteves de Lima <gabrielesteveslima@hotmail.com>
2022-05-30 16:06:44 -0300

    initial commit

---
+++ instructions.md
@@ -0,0 +1,167 @@
+# net-me-challenge-api
+
+Projeto de API com .NET Core 6
+
+## Sobre o projeto
+
+O proposito desse projeto é criar pedidos ("Orders") com alguns produtos já pre cadastrados na tabela ("Products").
+
+Após a criação das orders você pode alterar os dados de `itensAprovados` e `valorAprovado`
+que resultará em mudança de status da order.
+
+```
+public enum OrderStatus
+{
+    APROVADO = 1,
+    APROVADO_VALOR_A_MENOR = 2,
+    APROVADO_VALOR_A_MAIOR = 3,
+    APROVADO_QTD_A_MAIOR = 4,
+    APROVADO_QTD_A_MENOR = 5,
+    REPROVADO = 6,
+    CODIGO_PEDIDO_INVALIDO = 7
+}
+```
+
+## Base de dados
+
+Para simplificar o uso do projeto foi utilizado Ef Core 6 com Migrations em cima de uma banco Postgres.
+`As migration são executadas junto com o projeto.`
+
+![alt text for screen readers](C:\_\net-me-challenge-api\docs\postgres.PNG "postgres database").
+
+## Estrutura do projeto
+
+A solução possui alguns projetos, separando uma intenção de camada de dominio, digo intenção porque visto que é somente
+para aprendizado e o projeto não possui alta complexidade de negocio.
+
+Camadas de aplicação e infraestrutura e uma camada compartilhada chamada `SeedWorks`.
+
+![alt text for screen readers](C:\_\net-me-challenge-api\docs\project-tree.PNG "project tree").
+
+## Docker
+
+Para simplificar o teste da solução criei os arquivos do containers do docker, junto com o docker-compose, portanto para
+rodar a solução basta:
+
+``` bash
+docker-compose up -d --build --force-recreate
+```
+
+Feito isso o docker vai levantar os serviços configurados sendo eles:
+
+* O banco de dados postgres; e
+* A aplicação web api;
+
+![alt text for screen readers](C:\_\net-me-challenge-api\docs\docker-running.PNG "docker running").
+
+Assim temos os serviços de API e o banco rodando nas portas http/8080 e tcp/5432 respectivamente.
+
+** Caso o container ``mechallenge-api`` apresente erro de comunicação com o banco você pode iniciar ele manualmente;
+
+## Swagger
+Pode ser acessado via: http://localhost:8080/swagger/index.html
+
+## API
+
+A api está rodando com o ```Microsoft.AspNetCore.Mvc.Versioning``` uma lib para versionamento de recursos.
+
+Insira: `api-version = 1`
+
+### GET api/products
+
+Listagem de produtos
+
+response:
+
+```
+[
+  {
+    "produto": "05077f8d-a577-47d6-9c92-6714332950e4",
+    "nome": "Shorts",
+    "descricao": "Lorem Ipsum",
+    "precoUnitario": 95
+  },
+  {
+    "produto": "09d92788-b384-4e6d-bce2-fcb9cf1928e9",
+    "nome": "Glasses",
+    "descricao": "Lorem Ipsum",
+    "precoUnitario": 250
+  },
+]
+```
+
+### POST api/orders
+
+Criação de pedidos
+
+request:
+
+```
+{
+  "itens": [
+    {
+      "produto": "05077f8d-a577-47d6-9c92-6714332950e4",
+      "qtd": 20,
+    }
+  ]
+}
+```
+
+response body:
+
+````
+{
+  "pedido": "28dfd86c-b943-4da3-a521-f639a8cb4063",
+  "itens": [
+    {
+      "produto": "05077f8d-a577-47d6-9c92-6714332950e4",
+      "qtd": 20,
+      "precoUnitario": 95
+    }
+  ]
+}
+````
+
+### GET api/orders/{orderId}
+
+Listagem de pedidos
+
+response:
+
+```
+{
+  "pedido": "28dfd86c-b943-4da3-a521-f639a8cb4063",
+  "itens": [
+    {
+      "qtd": 20,
+      "descricao": "Lorem Ipsum",
+      "precoUnitario": 95
+    }
+  ]
+}
+```
+
+### GET api/status
+
+Alteração de status
+
+request:
+
+```
+{
+  "pedido": "182d151e-db1f-45df-a701-cf84e0f476b6",
+  "itensAprovados": 6,
+  "valorAprovado": 10
+}
+```
+
+response:
+
+```
+{
+  "pedido": "182d151e-db1f-45df-a701-cf84e0f476b6",
+  "status": "APROVADO_QTD_A_MENOR"
+}
+```
+
+É isso! Obrigado pela atenção.
---
+++ src/MeChallenge.Infrastructure/Migrations/20220530150729_InitialCreate.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeChallenge.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "MeChallenge");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                schema: "MeChallenge",
+                columns: table => new
+                {
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ValueApproved = table.Column<decimal>(type: "numeric", nullable: false),
+                    ItemsApproved = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "MeChallenge",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    UnitValue = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderProducts",
+                schema: "MeChallenge",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Value = table.Column<decimal>(type: "numeric", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderProducts", x => new { x.OrderId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_OrderProducts_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "MeChallenge",
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "MeChallenge",
+                table: "Products",
+                columns: new[] { "ProductId", "Description", "Name", "UnitValue" },
+                values: new object[,]
+                {
+                    { new Guid("05077f8d-a577-47d6-9c92-6714332950e4"), "Lorem Ipsum", "Shorts", 95m },
+                    { new Guid("09d92788-b384-4e6d-bce2-fcb9cf1928e9"), "Lorem Ipsum", "Glasses", 250m },
+                    { new Guid("58551994-7838-41c2-9928-480cc3bcf287"), "Lorem Ipsum", "Cap", 35m },
+                    { new Guid("8832c3fc-ca04-476d-8ced-4d400f5bdf07"), "Lorem Ipsum", "Jacket", 400m },
+                    { new Guid("98ecf0f5-53ff-48f3-80d0-048059357bec"), "Lorem Ipsum", "T-shirt", 50m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderProducts",
+                schema: "MeChallenge");
+
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "MeChallenge");
+
+            migrationBuilder.DropTable(
+                name: "Orders",
+                schema: "MeChallenge");
+        }
+    }
+}
